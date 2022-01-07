1ed2ea266b0385363b7dd14a51992cdb906838d8
Daniel <daniel.seidov@yandex.ru>
2022-01-07 06:17:29 +0300

    работает сохранение токена в localstorage

--- javascript/auth.js
+++ javascript/auth.js
@@ -3,23 +3,32 @@ document.querySelector("#submit").onclick = function(){
         "email": document.getElementById("email").value,
         "password": document.getElementById("password").value
     };
-    
-    (async () => {
-    let response = await fetch("/auth/sign-in", {
-        method: 'POST',
-        body: JSON.stringify(object) // <-- Post parameters        
-    })
-    let result = await response.json();
-    if (result.token != null) {
-        sessionStorage.setItem('token', result.token);
-        // const request = new XMLHttpRequest();
-        // request.open('GET', "/");
-        // request.setRequestHeader('Authorization', sessionStorage.token);
-        // request.send();
-    }
-    else {
-        alert("Неправильный email или пароль")
-    }
-    })()
 
+    sendRequest('POST', '/auth/sign-in', object)
+    .then(data => localStorage.setItem('token', JSON.stringify(data.token)))
+    .catch(err => console.log(err))
 };
+
+
+function sendRequest(method, url, params = null) {
+    return new Promise ( (resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+    xhr.open(method, url)
+    xhr.responseType = 'json'
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    
+    xhr.onload  = () => {
+        if(xhr.status >= 400) {
+            reject(xhr.response)
+        } else {
+            resolve(xhr.response)
+        }
+    }
+
+    xhr.onerror = () => {
+        reject(xhr.response)
+    }
+
+    xhr.send(JSON.stringify(params))
+    })
+}
