8818221621bb373ed1c4394b31b87f02e7f3a34d
Daniel <daniel.seidov@yandex.ru>
2022-01-13 05:38:49 +0300

    для heroku подключение к базе данных

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -21,10 +22,11 @@ var db *gorm.DB
 
 func Open_connection(cfg Config) (*gorm.DB, error) {
 	// строка соединения с базой данных
-	dbURI := fmt.Sprintf("host = %s user = %s dbname = %s sslmode = disable password = %s port = %s", cfg.Host, cfg.Username, cfg.DBName, cfg.Password, cfg.Port)
+	// dbURI := fmt.Sprintf("host = %s user = %s dbname = %s sslmode = disable password = %s port = %s", cfg.Host, cfg.Username, cfg.DBName, cfg.Password, cfg.Port)
+	connection := os.Getenv("DATABASE_URL")
 
 	// открываем соединение с базой данных
-	db, err := gorm.Open(postgres.Open(dbURI), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(connection), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error opening database connection: %s", err.Error())
 		return nil, err
