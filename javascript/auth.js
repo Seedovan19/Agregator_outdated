fa10e3e78406c1e58d9fe120c316e47e68b0c754
Daniel <daniel.seidov@yandex.ru>
2022-01-17 04:38:17 +0300

    асинхронный запрос к серверу

--- javascript/auth.js
+++ javascript/auth.js
@@ -4,38 +4,16 @@ document.querySelector("#submit").onclick = function(){
         "password": document.getElementById("password").value
     };
 
-    sendRequest('POST', '/auth/sign-in', object)
-    .then(data => {localStorage.token = "data.token";console.log(data.token)})
+    sendRequest('POST', '/auth/sign-in', object, true)
+    .then(data => {localStorage.token = data.token ;console.log(data.token)})
     .catch(err => console.log(err))
-    
-    localStorage.token = "data.token";
-
-    // new Promise ( (resolve, reject) => {
-    // const xhr = new XMLHttpRequest()
-    // xhr.open('GET', '/')
-    // xhr.setRequestHeader('Authorization', localStorage.token)
-    // xhr.send()
-    // xhr.onload  = () => {
-    //     if(xhr.status >= 400) {
-    //         reject(xhr.response)
-    //     }
-    //     else {
-    //         resolve(xhr.response)
-    //     }
-    // }
-
-    // xhr.onerror = () => {
-    //     reject(xhr.response)
-    // }
-    // }).then(result => html(result))
-    // .catch(err => console.log(err))
 };
 
 
-function sendRequest(method, url, params = null) {
+function sendRequest(method, url, params = null, asyncProc) {
     return new Promise ( (resolve, reject) => {
         const xhr = new XMLHttpRequest()
-    xhr.open(method, url)
+    xhr.open(method, url, !(!asyncProc))
     xhr.responseType = 'json'
     xhr.setRequestHeader('Content-Type', 'application/json')
     
